39847f531e26fb32391169f8e578da17b5f5a7b6
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-01-27 00:34:17 +0400

    added .env and port number customised and not found page added

---
+++ src/pages/not-found-page.tsx
@@ -0,0 +1,39 @@
+// import React from "react";
+// import { APP_NAME } from "@/lib/constants"; // Ensure the constant is available in your React project
+// import { Button } from "@/components/ui/button"; // Adjust the import based on your file structure
+
+// const NotFoundPage = () => {
+//   const handleRedirect = () => {
+//     window.location.href = "/";
+//   };
+
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen">
+//       <img
+//         src="/images/logo.svg"
+//         width={48}
+//         height={48}
+//         alt={`${APP_NAME} logo`}
+//       />
+//       <div className="p-6 w-1/3 rounded-lg shadow-md text-center">
+//         <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+//         <p className="text-destructive">Could not find requested page</p>
+//         <Button
+//           variant="outline"
+//           className="mt-4 ml-2"
+//           onClick={handleRedirect}
+//         >
+//           Back To Home
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NotFoundPage;
+
+const NotFoundPage = () => {
+  return <></>;
+};
+
+export default NotFoundPage;

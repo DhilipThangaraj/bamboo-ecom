84b6c46f02203008cd3129c7f5aa7510b81d0c55
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-01-26 23:46:54 +0400

    button comp added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div>hello world</div>;
+  return <Button>Click me</Button>;
 }
 
 export default App;
